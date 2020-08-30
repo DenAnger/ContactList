55e15af17147795eb8d08430665ccf9c95534c8c
Denis Abramov <denmiracle@mail.ru>
2020-08-30 16:19:17 +0700

    add contact manager
    add ContactManagerTests, ContactManager
    test init contact manager with empty list

---
+++ ContactList/Helprs/ContactManager.swift
@@ -0,0 +1,13 @@
+//
+//  ContactManager.swift
+//  ContactList
+//
+//  Created by Denis Abramov on 30.08.2020.
+//  Copyright © 2020 Denis Abramov. All rights reserved.
+//
+
+import Foundation
+
+class ContactManager {
+    let contactsCount = 0
+}
---
+++ ContactListTests/Helpers/ContactManagerTests.swift
@@ -0,0 +1,28 @@
+//
+//  ContactManagerTests.swift
+//  ContactListTests
+//
+//  Created by Denis Abramov on 30.08.2020.
+//  Copyright © 2020 Denis Abramov. All rights reserved.
+//
+
+import XCTest
+@testable import ContactList
+
+class ContactManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        
+        try super.tearDownWithError()
+    }
+    
+    func testInitContactManagerWithEmptyList() {
+        let contactManeger = ContactManager()
+        XCTAssertEqual(contactManeger.contactsCount, 0)
+    }
+
+}
