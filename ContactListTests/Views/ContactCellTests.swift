02d93790aed525925733f6f6699fff9aa5d6cdad
Denis Abramov <denmiracle@mail.ru>
2020-09-04 11:27:54 +0700

    refactoring

--- ContactListTests/Views/ContactCellTests.swift
+++ ContactListTests/Views/ContactCellTests.swift
@@ -11,48 +11,36 @@ import XCTest
 
 class ContactCellTests: XCTestCase {
     
+    var cell: ContactCell!
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(
+            identifier: "ContactListViewController"
+            ) as! ContactListViewController
+        viewController.loadViewIfNeeded()
+        
+        let tableView = viewController.tableView
+        let dataSource = MockTableViewDataSource()
+        tableView?.dataSource = dataSource
+        
+        cell = tableView?.dequeueReusableCell(
+            withIdentifier: "cell",
+            for: IndexPath(row: 0, section: 0)
+            ) as? ContactCell
     }
     
     override func tearDownWithError() throws {
-        
+        cell = nil
         try super.tearDownWithError()
     }
     
     func testCellHasNameLabel() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(
-            identifier: "ContactListViewController"
-            ) as! ContactListViewController
-        viewController.loadViewIfNeeded()
-        
-        let tableView = viewController.tableView
-        let dataSource = MockTableViewDataSource()
-        tableView?.dataSource = dataSource
-        
-        let cell = tableView?.dequeueReusableCell(
-            withIdentifier: "cell",
-            for: IndexPath(row: 0, section: 0)
-            ) as! ContactCell
         XCTAssertNotNil(cell.nameLabel)
     }
     
     func testCellHasNameLabelInContentView() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(
-            identifier: "ContactListViewController"
-            ) as! ContactListViewController
-        viewController.loadViewIfNeeded()
-        
-        let tableView = viewController.tableView
-        let dataSource = MockTableViewDataSource()
-        tableView?.dataSource = dataSource
-        
-        let cell = tableView?.dequeueReusableCell(
-            withIdentifier: "cell",
-            for: IndexPath(row: 0, section: 0)
-            ) as! ContactCell
         XCTAssertTrue(cell.nameLabel.isDescendant(of: cell.contentView))
     }
 }
